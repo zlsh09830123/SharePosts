5556147e9df234ac00a7b057c6685817e7d88f2b
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-12 19:27:08 +0800

    Custom flash messaging

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,7 @@
     require_once('config/config.php');
     // Load helpers
     require_once('helpers/url_helper.php');
+    require_once('helpers/session_helper.php');
 
     // Load Libraries
     // require_once('libraries/Core.php');
