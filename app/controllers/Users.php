76f389e09c71a3f58f5728e012166fe2d3051c2c
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-12 22:36:19 +0800

    User session data & Logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -134,7 +134,7 @@
 
                     if($loggedInUser) {
                         // Create Session
-                        die('success');
+                        $this->createUserSession($loggedInUser);
                     } else {
                         $data['password_err'] = 'Password incorrect';
 
@@ -157,4 +157,28 @@
                 $this->view('users/login', $data);
             }
         }
+
+        public function createUserSession($user) {
+            $_SESSION['user_id'] = $user->id;
+            $_SESSION['user_email'] = $user->email;
+            $_SESSION['user_name'] = $user->name;
+            redirect('pages/index');
+        }
+
+        public function logout() {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+            redirect('users/login');
+        }
+
+        public function isLoggedIn() {
+            if(isset($_SESSION['user_id'])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
+?>
