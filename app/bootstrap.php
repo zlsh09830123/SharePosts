95988291927d705578d6215abc9ca185d5e70ebd
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-12 18:08:54 +0800

    User registration

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
     // Load Config
     require_once('config/config.php');
+    // Load helpers
+    require_once('helpers/url_helper.php');
 
     // Load Libraries
     // require_once('libraries/Core.php');
