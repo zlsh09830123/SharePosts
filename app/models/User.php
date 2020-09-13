52a572e9d8e64eb7137e7805228ec1f0ef9d2ea7
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-13 17:19:35 +0800

    Post show details page

--- app/models/User.php
+++ app/models/User.php
@@ -53,5 +53,15 @@
             }
         }
 
+        // Get User by ID
+        public function getUserById($id) {
+            $this->db->query('SELECT * FROM users WHERE id=:id');
+            // Bind value
+            $this->db->bind(':id', $id);
+
+            $row = $this->db->single();
+
+            return $row;
+        }
     }
 ?>
