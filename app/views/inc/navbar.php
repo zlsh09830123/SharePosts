49475d9ef508bf52e962bdf083dccd39e56133c0
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-13 20:29:04 +0800

    Deleting posts

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
   <div class="container">
     <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,7 +17,7 @@
       <ul class="navbar-nav ml-auto">
         <?php if(isset($_SESSION['user_id'])): ?>
           <li class="nav-item">
-            <span class="navbar-text"><?php echo 'Hello! ' . $_SESSION['user_email']; ?></span>
+            <a class="nav-link" href="#">Welcome <?php echo $_SESSION['user_name']; ?></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
