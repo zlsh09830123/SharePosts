d5c6e9ee703a8e5c36444a122f2cfcb388966b90
zlsh09830123 <zlsh09830123@gmail.com>
2020-09-13 18:02:25 +0800

    Editing post

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -10,10 +10,10 @@
     <?php if($data['post']->user_id == $_SESSION['user_id']): ?>
         <hr>
         <a href="<?php echo URLROOT; ?>/posts/edit/<?php echo $data['post']->id; ?>" class="btn btn-dark">Edit</a>
-    <?php endif; ?>
 
-    <form class="pull-right" action="<?php echo URLROOT; ?>/posts/delete/<?php echo $data['post']->id; ?>" method="post">
-        <input type="submit" value="Delete" class="btn btn-danger">
-    </form>
+        <form class="pull-right" action="<?php echo URLROOT; ?>/posts/delete/<?php echo $data['post']->id; ?>" method="post">
+            <input type="submit" value="Delete" class="btn btn-danger">
+        </form>
+    <?php endif; ?>
 
 <?php require_once(APPROOT . '/views/inc/footer.php'); ?>
